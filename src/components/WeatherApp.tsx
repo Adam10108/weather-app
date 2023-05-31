9c641b61a34c1fd71058c606fd6c6eca2f9bc239
Adam10108 <adam_atlas1998@hotmail.com>
2023-05-31 16:24:50 +0700

    Simple get weather data from `API`

--- src/components/WeatherApp.tsx
+++ src/components/WeatherApp.tsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react"
+
+const config = {
+  base: import.meta.env.VITE_API_URL,
+  apiVersion: import.meta.env.VITE_API_VERSION,
+  apiKey: import.meta.env.VITE_API_KEY,
+}
+
 export const WeatherApp = () => {
+  const [query] = useState("bangkok")
+  const [_wether, setWeather] = useState()
+
+  useEffect(() => {
+    const getWeather = () => {
+      fetch(
+        `${config.base}/${config.apiVersion}/weather?q=${query}&appid=${config.apiKey}`
+      )
+        .then((resp) => resp.json())
+        .then((data) => setWeather(data))
+        .catch((error) => {
+          throw new Error(error)
+        })
+    }
+
+    getWeather()
+  }, [query])
+
   return <div>WeatherApp</div>
 }
