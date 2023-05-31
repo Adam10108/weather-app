2e50bb77cb6bd4483425b741b9ff2c873331b645
Adam10108 <adam_atlas1998@hotmail.com>
2023-05-31 17:46:41 +0700

    Display mock data

--- src/components/WeatherApp.tsx
+++ src/components/WeatherApp.tsx
@@ -7,23 +7,43 @@ const config = {
 }
 
 export const WeatherApp = () => {
-  const [query] = useState("bangkok")
+  const [query] = useState()
   const [_wether, setWeather] = useState()
 
-  useEffect(() => {
-    const getWeather = () => {
-      fetch(
-        `${config.base}/${config.apiVersion}/weather?q=${query}&appid=${config.apiKey}`
-      )
-        .then((resp) => resp.json())
-        .then((data) => setWeather(data))
-        .catch((error) => {
-          throw new Error(error)
-        })
-    }
+  // useEffect(() => {
+  //   const getWeather = () => {
+  //     fetch(
+  //       `${config.base}/${config.apiVersion}/weather?q=${query}&appid=${config.apiKey}`
+  //     )
+  //       .then((resp) => resp.json())
+  //       .then((data) => setWeather(data))
+  //       .catch((error) => {
+  //         throw new Error(error)
+  //       })
+  //   }
 
-    getWeather()
-  }, [query])
+  //   getWeather()
+  // }, [query])
 
-  return <div>WeatherApp</div>
+  return (
+    <div className='container'>
+      <div className='card-wrapper'>
+        <div className='card'>
+          <div className='search-container'>
+            <input className='search-bar' type='text' placeholder='Search...' />
+          </div>
+
+          <div className='location-container'>
+            <div className='label'>Bangkok, Thailand</div>
+            <div className='date'>May 31 2023</div>
+          </div>
+
+          <div className='weather-container'>
+            <div className='temperature'>30C</div>
+            <div className='weather'>Clouds</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
